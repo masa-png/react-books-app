f736cb08bd05b9f0501810834fafece615092659
masa-png <adidas.org@icloud.com>
2024-07-02 19:03:33 +0900

    新規登録エリアーの追加

--- resources/js/Pages/Books/Index.jsx
+++ resources/js/Pages/Books/Index.jsx
@@ -1,17 +1,24 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { useRef, useState } from "react";
+import PrimaryButton from "@/Components/PrimaryButton";
+import BlueButton from "@/Components/BlueButton";
+import GreenButton from "@/Components/GreenButton";
 import DangerButton from "@/Components/DangerButton";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
 import Modal from "@/Components/Modal";
 import SecondaryButton from "@/Components/SecondaryButton";
 import TextInput from "@/Components/TextInput";
+import TextareaInput from "@/Components/TextareaInput";
+import SelectBox from "@/Components/SelectBox";
 import { useForm } from "@inertiajs/react";
 
 export default function Index({ auth, books }) {
     const [confirmingUserDeletion, setConfirmingUserDeletion] = useState(false);
-    const passwordInput = useRef();
+    const titleInput = useRef();
+    const contentInput = useRef();
+    const categoryInput = useRef();
 
     const {
         data,
@@ -58,9 +65,102 @@ export default function Index({ auth, books }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <DangerButton onClick={confirmUserDeletion}>
-                        Delete Account
-                    </DangerButton>
+                    <BlueButton onClick={confirmUserDeletion}>登録</BlueButton>
+
+                    <Modal show={confirmingUserDeletion} onClose={closeModal}>
+                        <form onSubmit={deleteUser} className="p-6">
+                            <h2 className="text-lg font-medium text-gray-900">
+                                新しい本を追加しますか?
+                            </h2>
+
+                            <p className="mt-1 text-sm text-gray-600"></p>
+
+                            <div className="mt-6">
+                                <InputLabel
+                                    htmlFor="text"
+                                    value="title"
+                                    className="sr-only"
+                                />
+
+                                <TextInput
+                                    id="title"
+                                    type="text"
+                                    name="title"
+                                    ref={titleInput}
+                                    value={data.title}
+                                    onChange={(e) =>
+                                        setData("title", e.target.value)
+                                    }
+                                    className="mt-1 block w-3/4"
+                                    isFocused
+                                    placeholder="タイトル"
+                                ></TextInput>
+
+                                <InputError
+                                    message={errors.title}
+                                    className="mt-2"
+                                />
+                            </div>
+
+                            <div className="mt-6">
+                                <InputLabel
+                                    htmlFor="text"
+                                    value="content"
+                                    className="sr-only"
+                                />
+
+                                <TextareaInput
+                                    id="content"
+                                    type="text"
+                                    name="content"
+                                    ref={contentInput}
+                                    value={data.content}
+                                    onChange={(e) =>
+                                        setData("content", e.target.value)
+                                    }
+                                    className="mt-1 block w-3/4"
+                                    placeholder="内容"
+                                ></TextareaInput>
+
+                                <InputError
+                                    message={errors.content}
+                                    className="mt-2"
+                                />
+                            </div>
+
+                            <div className="mt-6">
+                                <SelectBox
+                                    name="category"
+                                    id="category"
+                                    ref={categoryInput}
+                                    value={data.category}
+                                    onChange={(e) =>
+                                        setData("category", e.target.value)
+                                    }
+                                    className="mt-1"
+                                    options={["", "React", "Vue", "Laravel"]}
+                                ></SelectBox>
+
+                                <InputError
+                                    message={errors.category}
+                                    className="mt-2"
+                                ></InputError>
+                            </div>
+
+                            <div className="mt-6 flex justify-end">
+                                <SecondaryButton onClick={closeModal}>
+                                    キャンセル
+                                </SecondaryButton>
+
+                                <BlueButton
+                                    className="ml-3"
+                                    disabled={processing}
+                                >
+                                    登録
+                                </BlueButton>
+                            </div>
+                        </form>
+                    </Modal>
 
                     <div>
                         <table className="w-full bg-gray-100 mt-2">
