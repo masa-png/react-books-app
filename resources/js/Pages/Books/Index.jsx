17f488e02ed77ebcaac58e469118914781ee3031
masa-png <adidas.org@icloud.com>
2024-07-02 18:00:08 +0900

    ボタンの追加

--- resources/js/Pages/Books/Index.jsx
+++ resources/js/Pages/Books/Index.jsx
@@ -58,9 +58,9 @@ export default function Index({ auth, books }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Books!</div>
-                    </div>
+                    <DangerButton onClick={confirmUserDeletion}>
+                        Delete Account
+                    </DangerButton>
 
                     <div>
                         <table className="w-full bg-gray-100 mt-2">
